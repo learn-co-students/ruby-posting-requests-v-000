4012627eda0a479f3d75e76ed35c93f8b5b5a0be
Chen Yang <yangc5@mail.uc.edu>
2016-03-13 20:54:04 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
       req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
       req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['redirect_uri'] = "http://yangc5-v-000-150060.nitrousapp.com:3000/auth"
       req.params['code'] = params[:code]
     end
 
