12e1c89132defc6d7e3af8fbac3ac47dd33024cf
sensei100 <jenanton01@gmail.com>
2016-09-27 17:45:24 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+FOURSQUARE = ENV["FOURSQUARE_ID"], ENV["FOURSQUARE_SECRET"]
 
 module ApisAndFaradayReading
   class Application < Rails::Application
