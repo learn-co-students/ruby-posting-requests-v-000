4dd5f2484500b26dd08fa7518e94b1cb1acd4abb
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-09 15:55:20 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     end
 
     body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
+    # session[:token] = body["access_token"]
     redirect_to root_path
   end
 end
