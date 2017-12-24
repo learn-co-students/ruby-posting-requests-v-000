0eed991f917fad1e1e777fcb33a3ea1bbb1db7c0
JingruZhang <zhang.jingru.93@gmail.com>
2017-12-24 12:56:17 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,17 @@ class SessionsController < ApplicationController
     session[:token] = body["access_token"]
     redirect_to root_path
   end
+
+  private
+
+  def authenticate_user
+    client_id = ENV['FOURSQUARE_CLIENT_ID']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    redirect_to foursquare_url unless logged_in?
+  end
+
+  def logged_in?
+    !!session[:token]
+  end
 end
