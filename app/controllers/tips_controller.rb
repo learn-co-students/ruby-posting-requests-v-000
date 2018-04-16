69528c3c0bc670257c9b75b5758018e1cec04cc2
Phoebe Lau <phoebelau7@gmail.com>
2018-04-16 16:37:55 +0000

    Done.

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,4 +1,16 @@
 class TipsController < ApplicationController
+<<<<<<< HEAD
+=======
+
+  def index
+    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['v'] = '20160201'
+    end
+    @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+  end
+
+>>>>>>> 1abd9e7a2069284dbaef4834139f8fed19d78b72
   def create
     resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
       req.params['oauth_token'] = session[:token]
@@ -10,6 +22,7 @@ class TipsController < ApplicationController
     redirect_to tips_path
   end
 
+<<<<<<< HEAD
   def index
     resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
       req.params['oauth_token'] = session[:token]
@@ -17,4 +30,6 @@ class TipsController < ApplicationController
     end
     @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
   end
+=======
+>>>>>>> 1abd9e7a2069284dbaef4834139f8fed19d78b72
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get '/auth', to: 'sessions#create'
   get '/friends', to: 'searches#friends'
   resources :tips, only: [:index, :create]
+<<<<<<< HEAD
   
+=======
+>>>>>>> 1abd9e7a2069284dbaef4834139f8fed19d78b72
 end
