7b50919d05185d2db800e43b173784688276777b
Allyson Wesman <allysonwesman@gmail.com>
2018-05-17 10:49:03 -0700

    Done.

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,31 @@
 class TipsController < ApplicationController
+  def index
+    @resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['v'] = '20160201'
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @tips = body["response"]["list"]["listItems"]["items"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+      render 'search'
+  end
+
+  def create
+    resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['v'] = '20160201'
+      req.params['venueId'] = params[:venue_id]
+      req.params['text'] = params[:tip]
+    end
+    byebug
+    redirect_to tips_path
+  end
 end
