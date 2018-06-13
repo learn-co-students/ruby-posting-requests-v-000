a9791d67b5331565376624149c60c68ef4d6c60a
github username <github email address>
2018-06-12 23:53:34 -0400

    trying

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,11 @@
 class TipsController < ApplicationController
+    def create
+      resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+        req.params['oauth_token'] = session[:token]
+        req.params['v'] = '20180614'
+        req.params['venue_id'] = params[:venue_id]
+        req.params['text'] = params[:tip]
+      end
+      redirect_to tips_path
+    end
 end
