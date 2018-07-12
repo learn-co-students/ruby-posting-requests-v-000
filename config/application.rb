a2a35ee706a2d56fceff23a2c60a60649fe68cdd
James Randolph <james.randolph2016@gmail.com>
2018-07-12 22:02:29 +0000

    Done.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'dotenv/load'
+
+task mytask: :dotenv do
+  
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
