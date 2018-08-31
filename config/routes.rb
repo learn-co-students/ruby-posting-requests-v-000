fdeefb5aced497329b31f2d58332f4ca0e318c71
Sebastian <sebgertz@gmail.com>
2018-08-31 14:37:11 -0400

    finished readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'searches#search'
+
+  resources :tips, only: [:index, :create]
+
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
 
