93260cbc6b33592d9a376c22e5763fb63243fa02
micahshute <micah.shute@gmail.com>
2019-01-10 16:05:51 -0600

    Done.

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,20 @@
 class TipsController < ApplicationController
+
+    def create
+        res = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req| 
+            req.params["oauth_token"] = session[:token]
+            req.params['v'] = '20160201'
+            req.params['venueId'] = params[:venue_id]
+            req.params['text'] = params[:tip]
+        end
+        redirect_to tips_path
+    end
+
+    def index
+        res = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+            req.params['oauth_token'] = session[:token]
+            req.params['v'] = '20160201'
+        end
+        @tips = JSON.parse(res.body)["response"]["list"]["listItems"]["items"]
+    end
 end
