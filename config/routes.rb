86e5b0af04cc785ac94b3327788a2a13d77f3564
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-01-29 12:11:22 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'searches#search'
+  resources :tips, only: [:index, :create]
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
 
   get '/auth', to: 'sessions#create'
   get '/friends', to: 'searches#friends'
+  
 
 end
