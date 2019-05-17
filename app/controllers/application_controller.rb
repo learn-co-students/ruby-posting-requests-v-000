7cdff8274f1a66e3901a211f84b5128e50ef4d8f
github username <github email address>
2019-05-16 18:24:23 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
+
 class ApplicationController < ActionController::Base
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -7,6 +10,8 @@ class ApplicationController < ActionController::Base
 private
 
   def authenticate_user
+
+
     client_id = ENV['FOURSQUARE_CLIENT_ID']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
+# require 'dotenv-rails'
+# Dotenv.load
+
+require 'pry'
+
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
       req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
