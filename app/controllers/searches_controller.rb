efb1b2760c2f0e4fc77a8557307c80257169b601
Erin <erin.y.green@gmail.com>
2019-06-11 16:25:28 -0400

    Adds .env file

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,12 @@
 class SearchesController < ApplicationController
   def search
   end
+
   def friends
     resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
       req.params['oauth_token'] = session[:token]
       # don't forget that pesky v param for versioning
-      req.params['v'] = '20160201'
+      req.params['v'] = '20190611'
     end
     @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
   end
