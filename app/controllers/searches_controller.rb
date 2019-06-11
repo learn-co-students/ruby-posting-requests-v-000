a045991ae94cfd1aa45d8e7154e0ac76728cc41e
Erin Green <erin.y.green@gmail.com>
2019-06-11 19:15:33 +0000

    fixes to gemfile

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,13 +11,13 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
-    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+    client_id = "GAKQ41HUMIUDU4T0PRJH5PUKIZMKQFIUN3DZRMPIJRBF0PUL"
+    client_secret = "OUED3CCEL5RRSPIPQX52GW1JPSGG2M0200MV30QOMTGFVAE4"
 
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
+      req.params['v'] = '20190611'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
