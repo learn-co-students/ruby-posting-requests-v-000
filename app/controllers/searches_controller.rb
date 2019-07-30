790735b1f3936bb4e430329f47c04c6e3b6f4a7b
github username <github email address>
2019-07-30 11:40:23 -0400

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,8 +11,8 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    client_id = ""
-    client_secret = ""
+    client_id = ENV['FOURSQUARE_CLIENT_ID']
+    client_secret = ENV['FOURSQUARE_SECRET']
 
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
